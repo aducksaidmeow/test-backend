bb8f72e35db9ef035b89b686b74e2d299075a4ca
aducksaidmeow <Duck7D6@gmail.com>
2022-11-03 20:48:34 +0700

    add cors

--- routes/api.route.js
+++ routes/api.route.js
@@ -4,4 +4,12 @@ router.get('/', async (req, res, next) => {
   res.send({ message: 'Ok api is working 🚀' });
 });
 
+router.post('/test', async(req, res, next) => {
+  try { 
+    res.send({ message: "Success!" });
+  } catch(error) {
+    next(error);
+  }
+});
+
 module.exports = router;
