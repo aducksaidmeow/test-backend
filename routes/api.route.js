b4fad31fb31d1a1b221876edd6ee8f115fef8dc1
aducksaidmeow <Duck7D6@gmail.com>
2022-11-03 21:31:51 +0700

    add cors

--- routes/api.route.js
+++ routes/api.route.js
@@ -1,4 +1,10 @@
 const router = require('express').Router();
+const cors = require('cors');
+
+router.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
 
 router.get('/', async (req, res, next) => {
   res.send({ message: 'Ok api is working 🚀' });
