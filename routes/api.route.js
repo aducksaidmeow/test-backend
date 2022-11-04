891f55fd4b9bf2cf7c6432a795ffd3e22729b9bc
aducksaidmeow <Duck7D6@gmail.com>
2022-11-04 23:08:10 +0700

    add oauth2Client and /api/get-token

--- routes/api.route.js
+++ routes/api.route.js
@@ -1,10 +1,19 @@
 const router = require('express').Router();
+const { google } = require('googleapis');
 const cors = require('cors');
 
 router.use(cors({
-  origin: "https://aducksaidmeow.github.io",
+  origin: "http://aducksaidmeow.github.io",
   credentials: true
 }));
+
+const oauth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.NODE_ENV === 'production' ? 
+    "http://aducksaidmeow.github.io" :
+    "http://localhost:3000"
+)
 
 router.get('/', async (req, res, next) => {
   res.send({ message: 'Ok api is working 🚀' });
@@ -13,6 +22,16 @@ router.get('/', async (req, res, next) => {
 router.post('/test', async(req, res, next) => {
   try { 
     res.send({ message: "Success!" });
+  } catch(error) {
+    next(error);
+  }
+});
+
+router.post('/get-token', async(req, res, next) => {
+  try {
+    const { code } = req.body;
+    const { tokens } = await oauth2Client.getToken(code);
+    res.send(tokens);
   } catch(error) {
     next(error);
   }
